f69dd0e0e17e6b30d5887abce58bb40ed0d5bd22
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-11-24 23:50:11 +0600

    set up express.js and connect with mongdb

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9ylutr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+      const database = client.db("destinyDuos");
+      const biodatas = database.collection("biodatas");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+      
+      
+      app.get('/biodatas', async (req, res) => {
+          const cursor = biodatas.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Destiny Duos is running");
+})
+
+app.listen(port, () => {
+    console.log(`Destiny Duos is running on port ${port}`);
+})
